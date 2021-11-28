c2b4aff3db3189a5e54ac2bfe164359a1229e04c
Charlotte Cameron <charlotte.cameron@adenin.com>
2021-11-28 12:55:54 +0000

    Memoize Firefly radius sizes

--- src/components/Contact/Fireflies.jsx
+++ src/components/Contact/Fireflies.jsx
@@ -5,8 +5,10 @@ import { fireflyWrapVariant } from 'components/common/constants';
 
 const Fireflies = (props) => {
   const [ref, animation] = InView('show', 'hidden');
+  // const radii = [...Array(9).keys()].map((i) => Math.floor((Math.random() * 4.5) + 5.5));
+  const radii = React.useMemo(() => [...Array(9).keys()].map((i) => Math.floor((Math.random() * 4.5) + 5.5)), []);
 
-  return (
+  return radii && (
     <motion.svg ref={ref} key='fireflies' variants={fireflyWrapVariant} initial="hidden" animate={animation} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 319" {...props}>
       <defs>
         <filter id="softGlow" height="300%" width="300%" x="-75%" y="-75%">
@@ -28,18 +30,18 @@ const Fireflies = (props) => {
         </radialGradient>
       </defs>
       <g filter="url(#softGlow)" shapeRendering="geometricPrecision">
-        <circle cx="927" cy="251" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{animation: 'hover 30s infinite linear alternate'}} />
-        <circle cx="985" cy="45" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 40s infinite linear alternate'}} />
-        <circle cx="1150" cy="121" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 20s infinite linear alternate'}} />
-        <circle cx="649.77" cy="130.84" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 25s infinite linear alternate'}} />
-        <circle cx="512" cy="35" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 55s infinite linear alternate'}} />
-        <circle cx="35" cy="55" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 10s infinite linear alternate'}} />
-        <circle cx="1283" cy="284" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 23s infinite linear alternate'}} />
-        <circle cx="180" cy="208" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 43s infinite linear alternate'}} />
-        <circle cx="418" cy="248" r={Math.floor((Math.random() * 4.5) + 5.5)} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 37s infinite linear alternate'}} />
+        <circle cx="927" cy="251" r={radii[0]} fill="url('#fireflyGradient')" style={{animation: 'hover 30s infinite linear alternate'}} />
+        <circle cx="985" cy="45" r={radii[1]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 40s infinite linear alternate'}} />
+        <circle cx="1150" cy="121" r={radii[2]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 20s infinite linear alternate'}} />
+        <circle cx="649.77" cy="130.84" r={radii[3]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 25s infinite linear alternate'}} />
+        <circle cx="512" cy="35" r={radii[4]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 55s infinite linear alternate'}} />
+        <circle cx="35" cy="55" r={radii[5]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 10s infinite linear alternate'}} />
+        <circle cx="1283" cy="284" r={radii[6]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 23s infinite linear alternate'}} />
+        <circle cx="180" cy="208" r={radii[7]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 43s infinite linear alternate'}} />
+        <circle cx="418" cy="248" r={radii[8]} fill="url('#fireflyGradient')" style={{offsetPath: `path('')`, animation: 'hover 37s infinite linear alternate'}} />
       </g>
     </motion.svg>
-  );
+  ) || null;
 };
 
 export default Fireflies;
