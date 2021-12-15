3c6b1c32533d2ad04481ab64e49046068d1ea6b3
Charlotte Cameron <charlotte.cameron@adenin.com>
2021-12-15 15:54:23 +0000

    Update contact button animation

--- src/components/Contact/ContactFly.jsx
+++ src/components/Contact/ContactFly.jsx
@@ -12,15 +12,7 @@ const ContactFly = ({ link, icon, x, y }) => {
       left={x}
       top={y}
       opacity="0.5"
-      whileHover={{
-        scale: 1.25,
-        opacity: 1,
-        transition: {
-          type: 'spring',
-          duration: 0.8,
-          bounce: 0.625
-        }
-      }}
+      whileHover={{ scale: 1.25, opacity: 1, transition: { duration: .5, ease: "backInOut" } }}
     >
       <Flex rounded="full" p="8" align="center" justify="center" bgGradient="radial(circle, #62336e 0%, #562A63 62.5%, #F9F9EF 75%)" filter="drop-shadow(0px 0px 12px #f2f2c8)">
         {icon}
