78233bb435592c7d8f7c4bce72abdad65dddbb7a
Charlotte Cameron <charlotte.cameron@adenin.com>
2021-12-19 20:54:22 +0000

    Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,57 @@
 import React from 'react';
-
-import { Link } from 'gatsby';
-import { Container } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { Container, SimpleGrid, Box, Flex, useBreakpointValue, Heading, Text, Link, Image } from '@chakra-ui/react';
 
 import SEO from '../components/common/seo';
+import Cursor from 'components/common/Cursor';
+import { Comets } from 'components/Animals/Comets';
+import StaggeredFade from 'components/common/StaggeredFade';
+import { itemVariant } from 'components/common/constants';
+import * as styles from "../styles/not-found.module.scss";
+import illustration from '../images/404-scene--min.svg';
+
+const MotionHeading = motion(Heading);
+const MotionText = motion(Text);
+const MotionLink = motion(Link);
+
+const variants = itemVariant;
 
 const PageNotFound = () => {
+  const showCursor = useBreakpointValue({ base: false, md: true })
+
   return (
     <>
       <SEO title="404: Page Not Found" />
-      <section id="hero" className="jumbotron">
-        <Container>
-          <h1 className="hero-title text-center">
-            Sorry, this path does not exist{' '}
-            <span role="img" aria-label="emoji">
-              😞
-            </span>
-          </h1>
-          <p className="hero-cta justify-content-center">
-            <Link className="cta-btn cta-btn--hero" to="/">
-              Go back
-            </Link>
-          </p>
-        </Container>
-      </section>
+      <Container maxW="100vw" h="100vh" className={styles.skyBG} position="relative" centerContent justifyContent='center'>
+        <Comets style={{position: "absolute", pointerEvents: "none"}} />
+        <SimpleGrid columns={{ md: 2 }} gap='12'>
+          <Box px={{base: '12', md: '6', xl: '12'}} zIndex='2'>
+            <Image src={illustration} maxH={{base: "50vh", md: "90vh", xl: "75vh"}} mx="auto" filter='drop-shadow(0px 12px 24px rgba(0,16,36,0.5))' />
+          </Box>
+          <Flex alignItems='center' textAlign={{base: "center", md: "left"}}>
+            <StaggeredFade>
+              <MotionHeading
+                fontWeight={700}
+                fontSize={{ base: '4xl', md: '5xl', xl: '7xl' }}
+                letterSpacing="1.5px"
+                lineHeight="1"
+                as="h1"
+                variants={variants}
+                mb="3"
+              >
+                All those who wander are not&nbsp;lost...
+              </MotionHeading>
+              <MotionText mb='6' variants={variants} textStyle='paragraph'>
+                ...but I'm afraid this page is. Follow the breadcrumbs to go back&nbsp;home.
+              </MotionText>
+              <MotionLink href="/" textStyle='paragraph' variants={variants}>
+              Back to the homepage
+              </MotionLink>
+            </StaggeredFade>
+          </Flex>
+        </SimpleGrid>
+      </Container>
+      {showCursor && <Cursor />}
     </>
   );
 };
