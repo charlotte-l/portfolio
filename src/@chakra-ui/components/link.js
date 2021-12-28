1b9c7c72ff38c07b48fea0688a739b5fc4b015a3
Charlotte Cameron <charlotte.cameron@adenin.com>
2021-12-28 11:32:38 +0000

    Update theme styles

--- src/@chakra-ui/components/link.js
+++ src/@chakra-ui/components/link.js
@@ -4,8 +4,12 @@ const Link = {
         color: "#ffe876",
         _hover: {
             textDecoration: 'none',
+            cursor: 'none !important',
+        },
+        _focus: {
+            boxShadow: 'none',
             cursor: 'none !important'
-        }
+        },
     }
 };
 
--- src/@chakra-ui/foundations/textStyles.js
+++ src/@chakra-ui/foundations/textStyles.js
@@ -1,8 +1,13 @@
 const textStyles = {
-  paragraph: {
+  "paragraph": {
     fontSize: ['18px', '20px'],
     fontWeight: 'light',
     letterSpacing: '0.5px',
+  },
+  "paragraph-sm": {
+    fontSize: ['16px', '18px'],
+    fontWeight: 'light',
+    letterSpacing: '0.35px',
   },
 };
 
