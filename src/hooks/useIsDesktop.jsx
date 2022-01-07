7114d46133acad9b5ae3ce88c6c073e78293963c
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-01-07 18:17:25 +0000

    Fix hook for SSR

--- src/hooks/useIsDesktop.jsx
+++ src/hooks/useIsDesktop.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useLayoutEffect } from "react";
 
-const getIsDesktop = () => window.innerWidth >= 1200;
+const getIsDesktop = () => {
+  if ("undefined" !== typeof window) {
+    return window.innerWidth >= 1280;
+  }
+  return undefined;
+}
 
 export default function useIsDesktop() {
   const [isDesktop, setIsDesktop] = useState(getIsDesktop());
