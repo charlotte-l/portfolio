cda53347eaa23607e4383e6898a64d65cf222c4f
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-01-15 15:54:56 +0000

    Add webpack bundle analyzer

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  const analyzerMode = process.env.INTERACTIVE_ANALYZE ? "server" : "json";
+
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode,
+        reportFileName: `./__build/bundlereport.json`,
+      }),
+    ],
   });
 };
