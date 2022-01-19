fd2fdbabd63a54a4dfa69bd73bab31fb30ef084b
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-01-19 22:09:15 +0000

    Remove unnecessary props attribute

--- src/components/common/StaggeredFade/index.jsx
+++ src/components/common/StaggeredFade/index.jsx
@@ -3,7 +3,7 @@ import { useInView } from 'react-intersection-observer';
 import { m, useAnimation } from 'framer-motion';
 import { wrapperVariant } from '../constants';
 
-const StaggeredFade = ({ children }, props) => {
+const StaggeredFade = ({ children }) => {
   const animation = useAnimation();
   const [ref, inView] = useInView({ threshold: 0.1, triggerOnce: true });
 
@@ -18,7 +18,7 @@ const StaggeredFade = ({ children }, props) => {
   const variants = wrapperVariant;
 
   return (
-    <m.div ref={ref} variants={variants} initial="hidden" animate={animation} {...props}>
+    <m.div ref={ref} variants={variants} initial="hidden" animate={animation}>
       {children}
     </m.div>
   );
